2a50ac7f0a856e32d4a7d23c6e1d04e7ee54417a
Ramrbs1 <86314526+Ramrbs1@users.noreply.github.com>
2021-08-24 11:47:54 +0530

    20

--- app.py
+++ app.py
@@ -4,6 +4,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Welcome, My name is Ram Kumar, This is Udacity final project Blue Deployment"
-
+    return "Welcome, My name is Ram Kumar, This is Udacity final project Green Deployment"
 app.run(host="0.0.0.0", port=8080, debug=True)
